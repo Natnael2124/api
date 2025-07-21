1248aebb8c6c49291b15d3ac5002c452a100d0d3
Natnael2124 <natnaelbajo24@gmail.com>
2025-07-21 21:04:07 +0300

    debug some errors

--- src/api.py
+++ src/api.py
@@ -10,8 +10,8 @@ def main():
 def get_artworks(query,limit):
     try:
         response = requests.get(
-            "https://api.artic.edu./api/v1/artworks/search",
-            {"q":query,"limit":limit}
+            "https://api.artic.edu/api/v1/artworks/search",
+            params = {"q":query,"limit":limit}
             )
         response.raise_for_status()
         content = response.json()
