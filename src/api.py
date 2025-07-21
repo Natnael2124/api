f1fe481019cd66c067eef78bd9d2819d50b71ab9
Natnael2124 <natnaelbajo24@gmail.com>
2025-07-21 21:46:54 +0300

    refactor : i have learnt about vim

--- src/api.py
+++ src/api.py
@@ -1,4 +1,4 @@
-import requests
+from artwork import get_artworks
 
 def main():
     artwork = input("enter an artwork name: ")
@@ -7,18 +7,7 @@ def main():
         print(f"*{artwork}")
 
 
-def get_artworks(query,limit):
-    try:
-        response = requests.get(
-            "https://api.artic.edu/api/v1/artworks/search",
-            params = {"q":query,"limit":limit}
-            )
-        response.raise_for_status()
-        content = response.json()
-        return [artwork["title"] for artwork in content["data"]]
-    except requests.HTTPError:
-        print("couldn't complete the request") 
-        return
+
 
     
 
